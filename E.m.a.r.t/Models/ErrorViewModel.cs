514855fa52482ad6c9ceb39d468c6da7cac664fd
tomas <tomas@PORTATIL_ASUS>
2025-05-07 10:05:15 +0100

    Adicionar arquivos de projeto.

---
+++ E.m.a.r.t/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E.m.a.r.t.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
